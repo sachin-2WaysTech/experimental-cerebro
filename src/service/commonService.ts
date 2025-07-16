a3f2a6aca5cd9c17b43b5b10ca043574f5b6f6e3
muhammad-2WaysTech <muhammad.ajmal@2ways.tech>
2025-07-16 19:07:53 +0530

    fix:response format change

--- src/service/commonService.ts
+++ src/service/commonService.ts
@@ -4,8 +4,8 @@ import type { NodeType } from "./nodeService";
 export async function getNodeTypes(): Promise<NodeType[]> {
   try {
     const response = await privateClient.get('/nodes/');
-    const { data, status } = response.data
-
+    const { data, status } = response
+    
     if (status) {
       return data
     } else {
