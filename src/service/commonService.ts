ada493f9f55ed69d6797b7fbb72d7bbcaf2bbaa9
muhammad-2WaysTech <muhammad.ajmal@2ways.tech>
2025-07-16 16:27:34 +0530

    fix:error in importing component

--- src/service/commonService.ts
+++ src/service/commonService.ts
@@ -1,9 +1,9 @@
-import { privateClinet } from "@/utils/privateClient";
+import { privateClient } from "@/utils/privateClient";
 import type { NodeType } from "./nodeService";
 
 export async function getNodeTypes(): Promise<NodeType[]> {
   try {
-    const response = await privateClinet.get('/nodes/');
+    const response = await privateClient.get('/nodes/');
     const { data, status } = response.data
 
     if (status) {
